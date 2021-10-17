1a14a737c0b7f0d739ca6464bdd0bf111538e563
shivamsingh124 <shivamsingh12042018@gmail.com>
2021-10-17 23:42:23 +0530

    basic cart

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Cart from "./views/cart/index";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
